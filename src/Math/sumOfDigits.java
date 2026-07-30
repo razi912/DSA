9c3e9f890a1fbfb73bc3ae04ff7cdba59f3a9dd7
Mohd Razi Abbas <md.raziabbas2006@gmail.com>
2026-07-30 23:22:25 +0530

    maths package added

---
+++ src/Math/sumOfDigits.java
@@ -0,0 +1,21 @@
+package Math;
+
+    import java.util.Scanner;
+
+    public class sumOfDigits {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int sum = 0;
+
+            while(n>0){
+                int d = n%10;
+                sum+=d;
+                n/=10;
+            }
+            System.out.println(sum);
+
+        }
+    }
+
+
