1c64d6688abaf1b72dadd24467ef401ef73421b2
Mohd Razi Abbas <md.raziabbas2006@gmail.com>
2026-08-09 13:45:25 +0530

    added pattern program

--- src/Patterns/RevNumPyramid.java
+++ src/Patterns/RevNumPyramid.java
@@ -4,8 +4,8 @@ package Patterns;
 public class RevNumPyramid {
     public static void main(String[] args) {
         int n = 5;
-        for(int i = n; i>=1; i--){
-            for(int j = 1; j<=i; j++ ){
+        for(int i = 1; i<=n; i++){
+            for(int j = n; j>=i; j-- ){
                 System.out.print(j);
             }
             System.out.println();
