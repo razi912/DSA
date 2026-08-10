b49ae426bc1d8b65c08345104b7761cc691e0bd5
Mohd Razi Abbas <md.raziabbas2006@gmail.com>
2026-08-10 10:57:31 +0530

    added pattern programs

---
+++ src/Patterns/OddStarPyramid.java
@@ -0,0 +1,16 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class OddStarPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i =2;i<=n;i+=2){
+            for(int j=2;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/Patterns/RepeatedNumPyramid.java
@@ -0,0 +1,9 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class RepeatedNumPyramid {
+    public static void main(String[] args) {
+
+    }
+}
