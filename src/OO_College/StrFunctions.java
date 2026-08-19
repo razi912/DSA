8dedc46d6b6847f264181d237a8caea21c7f0e28
Mohd Razi Abbas <md.raziabbas2006@gmail.com>
2026-08-19 23:19:43 +0530

    updated programs

---
+++ src/OO_College/StrFunctions.java
@@ -0,0 +1,42 @@
+package OO_College;
+
+public class StrFunctions {
+    public static void main(String[] args) {
+
+        String str = "Hello java";
+        System.out.println(str.charAt(3));
+        System.out.println(str.length());
+
+    }
+
+//    charAt()
+//    compareTo()
+//    ignoreCase()
+//    compareToIgnoreCase()
+//    concat()
+//    contains()
+//    copyValueOf()
+//    endsWith()
+//    equals()
+//    equalsIgnoreCase()
+//    indexOf()
+//    getChars()
+//    isEmpty()
+//    lastIndexOf()
+//    join()
+//    lastIndexOf()
+//    length()
+//    replace()
+//    split()
+//    replaceFirst
+//    startWith()
+//    subString()
+//    toCharArray()
+//    String.valueOf()
+
+
+
+
+
+
+}
