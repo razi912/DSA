db630e3a3f2fe6e314fd2ca2ef34435eb519abbc
Mohd Razi Abbas <md.raziabbas2006@gmail.com>
2026-08-30 08:16:06 +0530

    add reverse number pyramid pattern

--- src/O4_patterns/RevNumPyramid.java
+++ src/O4_patterns/RevNumPyramid.java
@@ -6,7 +6,7 @@ public class RevNumPyramid {
         int n = 5;
         for(int i = 1; i<=n; i++){
             for(int j = n; j>=i; j-- ){
-                System.out.print(j);
+                System.out.print(n-j+1);
             }
             System.out.println();
         }
