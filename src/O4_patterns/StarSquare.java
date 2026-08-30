340f24868a7e9aff111f3f02b3d5733f0e5f05d7
Mohd Razi Abbas <md.raziabbas2006@gmail.com>
2026-08-30 08:08:42 +0530

    made star square pattern

--- src/O4_patterns/StarSquare.java
+++ src/O4_patterns/StarSquare.java
@@ -3,15 +3,14 @@ package O4_patterns;
 import java.util.Scanner;
 
 public class StarSquare {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
-
     }
 }
