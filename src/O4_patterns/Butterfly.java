b26af58b91d057f4df0826c146b3198857883195
Mohd Razi Abbas <md.raziabbas2006@gmail.com>
2026-09-04 06:55:34 +0530

    made a star butterfly pattern

---
+++ src/O4_patterns/Butterfly.java
@@ -0,0 +1,40 @@
+package O4_patterns;
+
+import java.util.Scanner;
+
+public class Butterfly {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = n ; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n; i>=1; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
